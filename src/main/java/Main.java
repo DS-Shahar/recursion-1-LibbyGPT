e022c60fe69397e45fe6773527b16a39fd6cf15c
LibbyGPT <libibomiller@gmail.com>
2024-11-02 12:48:25 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -97,10 +97,60 @@ public class Main {
         return allEvenOrAllOddHelper(n / 10, parity);
     }
 
+    public static int sumArray(int[] arr, int index) {
+        if (index < 0) {
+            return 0;
+        }
+        return arr[index] + sumArray(arr, index - 1);
+    }
+
+    public static int countPositive(int[] arr, int index) {
+        if (index < 0) {
+            return 0;
+        }
+        int count = 0;
+        if (arr[index] > 0) {
+            count = 1;
+        }
+        return count + countPositive(arr, index - 1);
+    }
+
+    public static int findIndex(int[] arr, int target, int index) {
+        if (index == arr.length) {
+            return -1;
+        }
+        if (arr[index] == target) {
+            return index;
+        }
+        return findIndex(arr, target, index + 1);
+    }
+
+    public static boolean isSorted(int[] arr, int index) {
+        if (index == arr.length - 1) {
+            return true;
+        }
+        if (arr[index] > arr[index + 1]) {
+            return false;
+        }
+        return isSorted(arr, index + 1);
+    }
+
+    public static boolean hasNoPrimes(int[] arr, int index) {
+        if (index == arr.length) {
+            return true;
+        }
+        if (isPrime(arr[index])) {
+            return false;
+        }
+        return hasNoPrimes(arr, index + 1);
+    }
+
     public static void main(String[] args) {
         int n = 6;
         int number = 190350;
         int x = 20, y = 4;
+        int[] intArray = {1, 3, 5, 6};
+        int target = 5;
 
         System.out.println("Sum from 1 to " + n + ": " + sumToOne(n));
         System.out.println("Factorial of " + n + ": " + factorial(n));
@@ -111,5 +161,10 @@ public class Main {
         System.out.println(x + " is a multiple of " + y + ": " + isMultiple(x, y));
         System.out.println(n + " is prime: " + isPrime(n));
         System.out.println("All digits in " + number + " are even or all are odd: " + allEvenOrAllOdd(number));
+        System.out.println("Sum of array elements up to the given index: " + sumArray(intArray, 2));
+        System.out.println("Number of positive elements up to the given index: " + countPositive(intArray, 2));
+        System.out.println("Index of target element " + target + ": " + findIndex(intArray, target, 0));
+        System.out.println("Is the array sorted: " + isSorted(intArray, 0));
+        System.out.println("Has no prime numbers in array: " + hasNoPrimes(intArray, 0));
     }
 }
